5f353eb75ff1dc3119c7abcd0d6a4bfe2a88183c
Lyco <lyco@lyco.name>
2026-01-14 18:31:22 +0100

    cleaner music stream end handling

--- music_player.gd
+++ music_player.gd
@@ -5,11 +5,10 @@ var current_song_idx = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	self.stream = songs[current_song_idx]
+	self.play()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if not self.playing:
-		self.current_song_idx = (current_song_idx + 1) % len(songs)
-		self.stream = songs[current_song_idx]
-		self.play()
+func _on_finished() -> void:
+	self.current_song_idx = (current_song_idx + 1) % len(songs)
+	self.stream = songs[current_song_idx]
+	self.play()
