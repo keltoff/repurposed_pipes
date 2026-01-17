ab6d6c0daa685f83d3453cf13c267620f0fe8c94
Martin Dörfler <tryi.dent@gmail.com>
2026-01-17 15:28:25 +0100

    Center water source, some cleanup

--- shadowLabel.gd
+++ shadowLabel.gd
@@ -16,20 +16,25 @@ extends Label
 	set(x):
 		digits = x
 		update_text()
-		
+
+func _ready():
+	update_text()
+
+# I didn't fiund out how to make this work
 #@export_range(1, 50) var text_size : int = 10:
 	#set(x):
 		#text_size = x
 		#var theme_name = ''
+		
 		#if self.has_theme_font_size_override(theme_name):
 			#self.remove_theme_font_size_override(theme_name)
 			#$ValueLabel.remove_theme_font_size_override(theme_name)
 		#self.add_theme_font_size_override(theme_name, x)
 		#$ValueLabel.add_theme_font_size_override(theme_name, x)
 
-
 func update_text():
-	var txt_val = str(value)
-	if $ValueLabel:
-		$ValueLabel.text = prefix + txt_val.lpad(digits, ' ')
-	self.text = ' '.repeat(len(txt_val)).lpad(digits, '0')
+	if self.is_node_ready():
+		var txt_val = str(value)
+		if $ValueLabel:
+			$ValueLabel.text = prefix + txt_val.lpad(digits, ' ')
+		self.text = ' '.repeat(len(txt_val)).lpad(digits, '0')
